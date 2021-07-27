22379189b78a59f24fa2c50095f9e38634e358d1
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-07-27 16:59:36 +0200

    Adding explicit cookie csid, since header is not working on heroku

--- src/main/java/se/wigenstedt/finfollow/controllers/Login.java
+++ src/main/java/se/wigenstedt/finfollow/controllers/Login.java
@@ -90,6 +90,7 @@ public class Login {
                ResponseCookie cookie = ResponseCookie.from("csid", csid)
                        .httpOnly(true)
                        .path("/")
+                       .sameSite("none")
                        .build();
                return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).headers(headers1).body((String)result.get(0));
            }
