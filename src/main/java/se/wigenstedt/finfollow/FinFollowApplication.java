7b81dc521ac4d27d864ca258300b7b4904812be6
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-07-27 16:19:15 +0200

    Initial commit, and API endpoints

---
+++ src/main/java/se/wigenstedt/finfollow/FinFollowApplication.java
@@ -0,0 +1,13 @@
+package se.wigenstedt.finfollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinFollowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinFollowApplication.class, args);
+    }
+
+}
---
+++ src/main/java/se/wigenstedt/finfollow/constants/ApiEndpoints.java
@@ -0,0 +1,15 @@
+package se.wigenstedt.finfollow.constants;
+
+/**
+ * Created by Julia Wigenstedt
+ * Date: 2021-07-26
+ * Time: 13:18
+ * Project: FinFollow
+ * Copyright: MIT
+ */
+public class ApiEndpoints {
+
+    public static final String LOGIN = "authentication/sessions/bankid";
+    public static final String STATUS = "authentication/sessions/bankid/collect";
+    public static final String ACCOUNT_OVERVIEW = "account-overview/overview/categorizedAccounts";
+}
