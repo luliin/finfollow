6b9824e2059aa64560a4e5c3ae5937f87a7477b4
Julia Wigenstedt <juliaemmamatilda@gmail.com>
2021-08-17 12:47:35 +0200

    Add API endpoints for Nordnet

--- src/main/java/se/wigenstedt/finfollow/constants/ApiEndpoints.java
+++ src/main/java/se/wigenstedt/finfollow/constants/ApiEndpoints.java
@@ -12,4 +12,8 @@ public class ApiEndpoints {
     public static final String LOGIN = "authentication/sessions/bankid";
     public static final String STATUS = "authentication/sessions/bankid/collect";
     public static final String ACCOUNT_OVERVIEW = "account-overview/overview/categorizedAccounts";
+
+    public static final String NORDNET_SESSION = "https://www.nordnet.se/api/2/login/anonymous";
+    public static final String NORDNET_AUTOSTART_TOKEN = "https://www.nordnet.se/api/2/authentication/eid/se/bankid/start";
+    public static final String NORDNET_BANKID_POLL = "https://www.nordnet.se/api/2/authentication/eid/se/bankid/poll?order_ref=";
 }
